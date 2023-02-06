3b4163af86cfac7e347114a01f4bdfe499de55ba
Texa-Dev <artemtroc26@gmail.com>
2023-02-06 22:58:06 +0200

    add functions

--- src/pack/Fraction.java
+++ src/pack/Fraction.java
@@ -11,13 +11,38 @@ public class Fraction {
         this.dig = dig;
     }
 
-    public final IntSupplier getFlag=() -> flag;
-    public final IntConsumer setFlag=flag -> this.flag=flag;
-    public final IntSupplier getDig=() -> dig;
-    public final IntConsumer setDig=dig -> this.dig=dig;
+    public final IntSupplier getFlag = () -> flag;
+    public final IntConsumer setFlag = flag -> this.flag = flag;
+    public final IntSupplier getDig = () -> dig;
+    public final IntConsumer setDig = dig -> this.dig = dig;
+
+    public final Supplier<String> toString = () -> (this.getFlag.getAsInt() + "\\" + this.getDig.getAsInt()); //done
+    public final Supplier<Double> toDecimalFraction = () -> ((double) this.getFlag.getAsInt() / this.getDig.getAsInt()); //done
+
+    public final Predicate<Fraction> equals = frac -> (this.flag / this.dig) == (frac.flag / frac.dig);//done
+    public final UnaryOperator<Fraction> multy = frac -> new Fraction(this.flag * frac.flag, this.dig * frac.dig); //done
+    public final UnaryOperator<Fraction> dived = frac -> new Fraction(this.flag * frac.dig, this.dig * frac.flag);
+    public final UnaryOperator<Fraction> addition = frac -> {
+        int f = lcm(frac.dig, this.dig);
+        return new Fraction((this.flag * (f / this.dig)) + (frac.flag * (f / frac.dig)),
+                (this.dig * (f / this.dig)));
+    };
+    public final UnaryOperator<Fraction> sub = frac -> {
+        int f = lcm(frac.dig, this.dig);
+        return new Fraction((this.flag * (f / this.dig)) - (frac.flag * (f / frac.dig)),
+                (this.dig * (f / this.dig)));
+    };
 
     public static final BinaryOperator<Fraction> multyFrac =
-            (f1,f2)->new Fraction(f1.flag* f2.flag, f1.dig* f2.dig );
+            (f1, f2) -> new Fraction(f1.flag * f2.flag, f1.dig * f2.dig);
+    public static final Function<Fraction, Double> decimal = p -> (double) p.flag / p.dig;
 
-    public final Supplier<String> toString = () ->( this.getFlag.getAsInt() +"\\"+ this.getDig.getAsInt());
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    int lcm(int a, int b) {
+        return a / gcd(a, b) * b;
+    }
+
 }
--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,14 +1,26 @@
 package pack;
 
+
+
 public class Main {
     public static void main(String[] args) {
+
         Fraction fraction = new Fraction(1, 2);
         Fraction fraction2 = new Fraction(2, 3);
+        Fraction fraction3 = new Fraction(4, 6);
+        Fraction fraction4 = new Fraction(2, 5);
 
-        System.out.println( fraction.toString.get());
-        fraction.setFlag.accept(5);
-        System.out.println( fraction.toString.get());
-        System.out.println( fraction2.toString.get());
-        System.out.println(Fraction.multyFrac.apply(fraction,fraction2).toString.get());
+       fraction= fraction.sub.apply(fraction3);
+        System.out.println(fraction.toString.get());
+
+//        fraction.setFlag.accept(5);
+//        System.out.println( fraction.toString.get());
+//        System.out.println( fraction2.toString.get());
+//        System.out.println( fraction2.toDecimalFraction.get());
+//        System.out.println(Fraction.multyFrac.apply(fraction,fraction2).toString.get());
+//
+//        System.out.println(fraction4.equals.test(fraction3));
+//
+//        System.out.println(Fraction.decimal.apply(fraction));
     }
 }
