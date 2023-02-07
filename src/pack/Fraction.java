26aaf484513dcdbdeb65e9311e75466d855f8235
Texa-Dev <artemtroc26@gmail.com>
2023-02-08 01:18:51 +0200

    refactor, test

--- src/pack/Fraction.java
+++ src/pack/Fraction.java
@@ -11,37 +11,67 @@ public class Fraction {
         this.dig = dig;
     }
 
+    //геттеры  сеттеры
     public final IntSupplier getFlag = () -> flag;
     public final IntConsumer setFlag = flag -> this.flag = flag;
     public final IntSupplier getDig = () -> dig;
     public final IntConsumer setDig = dig -> this.dig = dig;
 
-    public final Supplier<String> toString = () -> (this.getFlag.getAsInt() + "\\" + this.getDig.getAsInt()); //done
-    public final Supplier<Double> toDecimalFraction = () -> ((double) this.getFlag.getAsInt() / this.getDig.getAsInt()); //done
+    //преобразование дроби в строку "1/2"
+    public final Supplier<String> toString = () -> (this.flag + "/" + this.dig); //done
 
-    public final Predicate<Fraction> equals = frac -> (this.flag / this.dig) == (frac.flag / frac.dig);//done
-    public final UnaryOperator<Fraction> multy = frac -> new Fraction(this.flag * frac.flag, this.dig * frac.dig); //done
+    //преобразование дроби в десятичную 1/2 –> 0.5
+    public final Supplier<Double> toDecimalFraction = () -> ((double) this.flag / this.dig); //done
+
+    // сравнение этой дроби с другой
+    public final Predicate<Fraction> equals = frac -> (decimal.apply(this).equals(decimal.apply(frac)));//done
+    //умножение этой дроби на другую
+    public final UnaryOperator<Fraction> multi = frac -> new Fraction(this.flag * frac.flag, this.dig * frac.dig); //done
+
+    //деление этой дроби на другую
     public final UnaryOperator<Fraction> dived = frac -> new Fraction(this.flag * frac.dig, this.dig * frac.flag);
+
+    //суммирование этой дроби с другой
     public final UnaryOperator<Fraction> addition = frac -> {
         int f = lcm(frac.dig, this.dig);
         return new Fraction((this.flag * (f / this.dig)) + (frac.flag * (f / frac.dig)),
                 (this.dig * (f / this.dig)));
     };
-    public final UnaryOperator<Fraction> sub = frac -> {
+    //отнимание другой дроби от этой
+    public final UnaryOperator<Fraction> subtraction = frac -> {
         int f = lcm(frac.dig, this.dig);
         return new Fraction((this.flag * (f / this.dig)) - (frac.flag * (f / frac.dig)),
                 (this.dig * (f / this.dig)));
     };
 
-    public static final BinaryOperator<Fraction> multyFrac =
-            (f1, f2) -> new Fraction(f1.flag * f2.flag, f1.dig * f2.dig);
+
+    //преобразование дроби в десятичную 1/2 -> 0.5
     public static final Function<Fraction, Double> decimal = p -> (double) p.flag / p.dig;
 
-    int gcd(int a, int b) {
+    //сравнение двух дробей
+    public static final BiPredicate<Fraction, Fraction> equalsTwoFrac = (frac1, frac2) -> decimal.apply(frac1).equals(decimal.apply(frac2));
+
+    //преобразовать строку в дробь. Строка должна иметь вид: "1/2".
+    public static final Function<String, Fraction> stringToFrac = s -> {
+        String[] split = s.split("/");
+        return new Fraction(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+    };
+
+    //умножение двух дробей
+    public static final BinaryOperator<Fraction> multiTwoFrac =
+            (f1, f2) -> new Fraction(f1.flag * f2.flag, f1.dig * f2.dig);
+
+    //деление двух дробей
+    public static final BinaryOperator<Fraction> divedTwoFrac =
+            (f1, f2) -> new Fraction(f1.flag * f2.dig, f1.dig * f2.flag);
+
+
+    //функции приведения к общему знаменателю
+    private int gcd(int a, int b) {
         return b == 0 ? a : gcd(b, a % b);
     }
 
-    int lcm(int a, int b) {
+    private int lcm(int a, int b) {
         return a / gcd(a, b) * b;
     }
 
--- src/pack/Main.java
+++ src/pack/Main.java
@@ -5,22 +5,52 @@ package pack;
 public class Main {
     public static void main(String[] args) {
 
-        Fraction fraction = new Fraction(1, 2);
-        Fraction fraction2 = new Fraction(2, 3);
-        Fraction fraction3 = new Fraction(4, 6);
-        Fraction fraction4 = new Fraction(2, 5);
+        Fraction testF = new Fraction(2, 6);
+        Fraction f = new Fraction(1, 2);
+        Fraction f2 = new Fraction(1, 3);
 
-       fraction= fraction.sub.apply(fraction3);
-        System.out.println(fraction.toString.get());
 
-//        fraction.setFlag.accept(5);
-//        System.out.println( fraction.toString.get());
-//        System.out.println( fraction2.toString.get());
-//        System.out.println( fraction2.toDecimalFraction.get());
-//        System.out.println(Fraction.multyFrac.apply(fraction,fraction2).toString.get());
-//
-//        System.out.println(fraction4.equals.test(fraction3));
-//
-//        System.out.println(Fraction.decimal.apply(fraction));
+        //setters
+//        testF.setFlag.accept(3);
+//        testF.setDig.accept(4);
+//        System.out.println(testF.toString.get());
+
+        //fraction to decimal
+        System.out.println(f.toDecimalFraction.get());
+
+        //equals
+        System.out.println(testF.equals.test(f));
+        System.out.println(f.equals.test(f2));
+
+        //multi
+        System.out.println(f.multi.apply(testF).toString.get());
+
+        //dived
+        System.out.println(f.dived.apply(testF).toString.get());
+
+        //addition
+        System.out.println(f.addition.apply(f2).toString.get());
+
+        //subtraction
+        System.out.println(testF.subtraction.apply(f2).toString.get());
+
+        //Static methods
+
+        //to Decimal
+        System.out.println(Fraction.decimal.apply(testF));
+
+        //equalsTwoFrac
+        System.out.println(Fraction.equalsTwoFrac.test(f,f2));
+        System.out.println(Fraction.equalsTwoFrac.test(testF,f2));
+
+        //string to Fraction
+        Fraction f3 = Fraction.stringToFrac.apply("125/247");
+        System.out.println( f3.toString.get() +" "+ f3.getClass());
+
+        //multiTwoFrac
+        System.out.println(Fraction.multiTwoFrac.apply(f,f2).toString.get());
+
+        //divedTwoFrac
+        System.out.println(Fraction.divedTwoFrac.apply(f,f2).toString.get());
     }
 }
